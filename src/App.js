1c9f2881391ae391e430404e86f08e349d371aa7
brianmazzocchi <66741736+brianmazzocchi@users.noreply.github.com>
2021-04-07 15:05:48 -0300

    Finalizando proyecto

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, {Fragment, useState, useEffect} from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
+import ListadoNoticias from './components/ListadoNoticias';
 
 function App() {
 
   //Definir la categoria y noticias
   const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
 
   useEffect(()=> {
     const consultarAPI = async () => {
       const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=8e04f17d896248ee8d97596441cfcbe9`
       const respuesta = await fetch(url);
       const noticias = await respuesta.json();
-      console.log(categoria);
-      console.log(noticias.articles);
+      guardarNoticias(noticias.articles);
+      
     
     };
     consultarAPI();
@@ -28,6 +30,10 @@ function App() {
       <div className="container-white">
         <Formulario
           guardarCategoria={guardarCategoria}
+        />
+
+        <ListadoNoticias
+          noticias={noticias}
         />
       </div>
 
